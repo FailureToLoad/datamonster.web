f34e00a3eb1449b221eff93999be19220402d88b
DMason <davejmason@outlook.com>
2024-07-24 00:24:37 -0600

    eliminates value loading from layout
    this was causing issues with routing. removing the title is temporary.

--- app/(protected)/settlements/[settlementId]/layout.tsx
+++ app/(protected)/settlements/[settlementId]/layout.tsx
@@ -1,21 +1,11 @@
-import { Settlement } from "@/lib/types/settlements";
 import Link from "next/link";
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
-import { GetSettlement } from "./actions";
 
-interface HeaderProps {
-  settlement: Settlement;
-}
-function Header({ settlement }: HeaderProps) {
+function Header() {
   return (
     <div id="header" className="sticky top-0 w-full flex-none">
       <div className="m-2 flex h-20 justify-center">
         <div className="flex w-1/3 flex-col">
-          <div className="flex w-full">
-            <div className="inline-flex grow justify-center">
-              {settlement?.name}
-            </div>
-          </div>
           <div className="inline-flex justify-center">
             <Link href="timeline/" className={navigationMenuTriggerStyle()}>
               Timeline
@@ -35,17 +25,12 @@ function Header({ settlement }: HeaderProps) {
 
 export default async function SettlementLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: {
-    settlementId: string;
-  };
 }) {
-  const settlement = await GetSettlement(params.settlementId);
   return (
     <div className="flex h-screen w-full flex-col">
-      <Header settlement={settlement} />
+      <Header />
       <div className="p-16">{children}</div>
     </div>
   );
