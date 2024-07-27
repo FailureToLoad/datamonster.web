7f3c6bd42775c4c9fbd32890deade0bbe89e146b
DMason <davejmason@outlook.com>
2024-07-27 01:34:23 -0600

    adds custom themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,5 +11,65 @@ module.exports = {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            background: "#ffffff",
+            foreground: "#000000",
+            primary: {
+              DEFAULT: "#6B7280",
+              foreground: "#ffffff",
+            },
+            secondary: {
+              DEFAULT: "#0792FC",
+              foreground: "#ffffff",
+            },
+            success: {
+              DEFAULT: "#36C164",
+              foreground: "#ffffff",
+            },
+            warning: {
+              DEFAULT: "#DB9729",
+              foreground: "#ffffff",
+            },
+            danger: {
+              DEFAULT: "#FF6159",
+              foreground: "#ffffff",
+            },
+            focus: "#4433FF",
+          },
+        },
+        dark: {
+          colors: {
+            background: "#000000",
+            foreground: "#ffffff",
+            primary: {
+              DEFAULT: "#6B7280",
+              foreground: "#000000",
+            },
+            secondary: {
+              DEFAULT: "#0792FC",
+              foreground: "#000000",
+            },
+            success: {
+              DEFAULT: "#36C164",
+              foreground: "#000000",
+            },
+            warning: {
+              DEFAULT: "#DB9729",
+              foreground: "#000000",
+            },
+            danger: {
+              DEFAULT: "#FF6159",
+              foreground: "#000000",
+            },
+            focus: "#4433FF",
+          },
+          // ... rest of the colors
+        },
+      },
+    }),
+  ],
 };
