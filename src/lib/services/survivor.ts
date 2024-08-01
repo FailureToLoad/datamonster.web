6b9f3737d0731bf4ab61a7f4ca8a1767d67bc1df
DMason <davejmason@outlook.com>
2024-08-01 00:52:23 -0600

    brings back call to get survivors

--- src/lib/services/survivor.ts
+++ src/lib/services/survivor.ts
@@ -1,7 +1,10 @@
 import { Survivor } from "@/lib/types/survivor";
 
-export async function CreateSurvivor(survivor: Survivor, settlementId: string) {
-  const accessToken = "update token logic";
+export async function CreateSurvivor(
+  survivor: Survivor,
+  settlementId: string,
+  accessToken: string
+) {
   const req = `${
     import.meta.env.VITE_API_HOST
   }/settlements/${settlementId}/survivors`;
@@ -17,4 +20,28 @@ export async function CreateSurvivor(survivor: Survivor, settlementId: string) {
   if (!response.ok) {
     throw new Error("survivor creation failed");
   }
+}
+
+export async function FetchSurvivors(
+  settlementId: string,
+  accessToken: string
+) {
+  const path = `${
+    import.meta.env.VITE_API_HOST
+  }/settlements/${settlementId}/survivors`;
+  console.log(`request path ${path}`);
+  const response = await fetch(path, {
+    method: "get",
+    headers: new Headers({
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("unable to fetch survivors");
+  }
+
+  const responseJson: Array<Survivor> = await response.json();
+  return responseJson;
 }
